aa74cc0caab7c8b479989020c099644c429164ef
foreverilone <ilona-lang@mail.ru>
2021-07-12 20:49:29 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import MainContainer from '../components/MainContainer';
+
+const Index = ({ keywords }) => {
+  return (
+      <MainContainer keywords={"main page"}>
+        
+        <h1>Главная страница</h1>
+        
+      </MainContainer>
+  )
+};
+
+export default Index;
---
+++ pages/users.js
@@ -0,0 +1,32 @@
+import Link from 'next/Link';
+import MainContainer from '../components/MainContainer';
+
+const Users = ({ users, keywords }) => {
+
+  return (
+    <MainContainer keywords={"users page"}>
+      <h1>Список пользователей</h1>
+      <ul>
+        {users.map(user =>
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>
+              <a>{user.name}</a>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </MainContainer>
+  );
+};
+
+export default Users;
+
+
+export async function getStaticProps(context) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const users = await response.json();
+
+  return {
+    props: {users}, // will be passed to the page component as props
+  }
+}
